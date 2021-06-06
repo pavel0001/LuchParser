7984ebbaaf6f12ce344f795fec5bcae3c851e955
Pavel <p.valtorn@gmail.com>
2021-06-06 23:33:17 +0300

    add navigation

---
+++ app/src/main/java/by/valtorn/luchparser/ui/root/ui/RootFragment.kt
@@ -0,0 +1,13 @@
+package by.valtorn.luchparser.ui.root.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.valtorn.luchparser.R
+
+
+class RootFragment : Fragment() {
+
+}
